6754151e10570936c2ae4d8d9a3df483557e935e
Aya Abdelkhalk <aya.abdelkhalk30@gmail.com>
2024-01-28 01:21:13 -0800

    aa

---
+++ ELearn.InfraStructure/Configurations/QuestionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ELearn.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELearn.InfraStructure.Configurations
+{
+    public class QuestionConfiguration : IEntityTypeConfiguration<Question>
+    {
+        public void Configure(EntityTypeBuilder<Question> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ELearn.InfraStructure/Configurations/QuizConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ELearn.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELearn.InfraStructure.Configurations
+{
+    public class QuizConfiguration : IEntityTypeConfiguration<Quiz>
+    {
+        public void Configure(EntityTypeBuilder<Quiz> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
