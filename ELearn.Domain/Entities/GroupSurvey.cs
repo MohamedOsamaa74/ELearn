e152728032097bf2b2d8de0d0666d2cd8124dec9
MATRIX <MATRIX@DESKTOP-PQOAC0F>
2024-01-29 00:11:16 +0200

    dd

--- ELearn.Domain/Entities/GroupSurvey.cs
+++ ELearn.Domain/Entities/GroupSurvey.cs
@@ -2,7 +2,11 @@
 {
     public class GroupSurvey
     {
-        public int GroupsurveyId { get; set; }
-        public int GroupId { get; set; }
-    }
+        // public int GroupsurveyId { get; set; }
+        public required int GroupId { get; set; }
+        public required int SyrveyId { get; set; }
+        public virtual Group Group { get; set; }
+        public virtual Voting Survey { get; set; }
+
+    }  
 }
