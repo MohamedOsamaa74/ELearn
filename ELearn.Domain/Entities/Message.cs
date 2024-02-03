122df598f932cddb83b74753d97c9e92e033b363
Aya Abdelkhalk <aya.abdelkhalk30@gmail.com>
2024-02-02 23:04:57 -0800

    Add Message Relation

--- ELearn.Domain/Entities/Message.cs
+++ ELearn.Domain/Entities/Message.cs
@@ -7,13 +7,12 @@ namespace ELearn.Domain.Entities
         public int MessageId { get; set; }
         //senderid
         //[ForeignKey("Sender")]
-        //public required int SenderId { get; set; }
-        //[ForeignKey("Receiver")]
-        //public required int ReceiverId { get; set; }
+        public required string SenderId { get; set; }
+        public required string ReceiverId { get; set; }
         public required string Text { get; set; }
         public required DateTime Date { get; set; }=DateTime.Now;
-        //public virtual ApplicationUser Sender { get; set; }
-        //public virtual ApplicationUser Receiver { get; set; }
+        public virtual ApplicationUser Sender { get; set; }
+        public virtual ApplicationUser Receiver { get; set; }
 
     }
 }
--- ELearn.InfraStructure/Configurations/MessageConfiguration.cs
+++ ELearn.InfraStructure/Configurations/MessageConfiguration.cs
@@ -16,13 +16,13 @@ namespace ELearn.InfraStructure.Configurations
             builder.ToTable("Messages");
             builder.HasKey(x => x.MessageId);
 
-        //    builder.HasOne(m => m.Sender)
-        //        .WithMany(u => u.SentMessages)
-        //        .HasForeignKey(m => m.SenderId);
+            builder.HasOne(m => m.Sender)
+                .WithMany(u => u.SentMessages)
+                .HasForeignKey(m => m.SenderId);
 
-        //    builder.HasOne(m => m.Receiver)
-        //        .WithMany(u => u.ReceivedMessages)
-        //        .HasForeignKey(m => m.ReceiverId);
+            builder.HasOne(m => m.Receiver)
+                .WithMany(u => u.ReceivedMessages)
+                .HasForeignKey(m => m.ReceiverId);
 
         }
     }
