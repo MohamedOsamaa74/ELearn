58b85b1f89b0851034dd9b910489ceab6041c0ee
MATRIX <MATRIX@DESKTOP-PQOAC0F>
2024-03-19 12:59:49 +0200

    dd

--- ELearn.Application/Helpers/AutoMapper/AutoMapperProfile.cs
+++ ELearn.Application/Helpers/AutoMapper/AutoMapperProfile.cs
@@ -45,7 +45,6 @@ namespace ELearn.Application.Helpers.AutoMapper
                 .ForMember(dest => dest.UserId, opt => opt.Ignore());
 
 
-
             CreateMap<Material, UpdateMaterialDTO>()
             .ForMember(dest => dest.File, opt => opt.Ignore())
             .ForMember(dest => dest.Link, opt => opt.Ignore());
