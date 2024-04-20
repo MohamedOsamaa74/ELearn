9f91850390012e710f8d0ec513ec423d036e3905
Aya Abdelkhalk <aya.abdelkhalk30@gmail.com>
2024-04-20 02:36:54 +0200

    Create Quiz

---
+++ ELearn.Api/Controllers/QuizController.cs
@@ -0,0 +1,28 @@
+﻿using ELearn.Application.DTOs.QuizDTOs;
+using ELearn.Application.Helpers.Response;
+using ELearn.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELearn.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        private readonly IQuizService _quizService;
+        public QuizController(IQuizService QuizService)
+        {
+            _quizService = QuizService;
+        }
+        [HttpPost("CreateNewQuiz")]
+        [Authorize(Roles = "Admin ,Staff")]
+        public async Task<IActionResult> CreateQuiz([FromBody] CreateQuizDTO Model)
+        {
+            var response = await _quizService.CreateNewAsync(Model);
+            return this.CreateResponse(response);
+
+        }
+    }
+}
--- ELearn.Application/DTOs/QuestionDTOs/CreateQuestionDTO.cs
+++ ELearn.Application/DTOs/QuestionDTOs/CreateQuestionDTO.cs
@@ -2,8 +2,15 @@
 {
     public class CreateQuestionDTO
     {
-        public required string Text { get; set; }
-        public ICollection<string> Options { get; set; } = [];
-        public string? CorrectOption { get; set; }
+        public string Text { get; set; }
+        //file
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+        public string Option3 { get; set; }
+        public string Option4 { get; set; }
+        public string Option5 { get; set; }
+        public string CorrectAnswer { get; set; }
+        public int Grade { get; set; }
+
     }
 }
